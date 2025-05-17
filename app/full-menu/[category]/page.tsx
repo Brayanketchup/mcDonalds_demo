1473872478f4cda3a9ba01f34bd86f0b5b1c26aa
Brayanketchup <112900383+Brayanketchup@users.noreply.github.com>
2025-05-16 20:17:17 -0400

    last push

--- app/full-menu/[category]/page.tsx
+++ app/full-menu/[category]/page.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import { fetchProductsByCategory } from '@/utils';
-import { CategoriesDisplay } from '@/components'
-import Image from 'next/image';
-import Link from 'next/link';
+import React from "react";
+import { fetchProductsByCategory } from "@/utils";
+import { CategoriesDisplay } from "@/components"
+import Image from "next/image";
+import Link from "next/link";
 
 interface PageProps {
     params: {
@@ -19,24 +19,24 @@ const Page = async ({ params }: PageProps) => {
     
     return (
         <section>
-            <h1 className='text-4xl md:text-5xl lg:text-6xl w-full text-center py-5'>{category}</h1>
-            <div className='flex flex-row p-5'>
-                <div className='md:w-1/3 w-full md:flex hidden justify-center h-fit'>
+            <h1 className="text-4xl md:text-5xl lg:text-6xl w-full text-center py-5">{category}</h1>
+            <div className="flex flex-row p-5">
+                <div className="md:w-1/3 w-full md:flex hidden justify-center h-fit">
                     <CategoriesDisplay 
-                    aditionalPath={'full-menu'}
+                    aditionalPath={"full-menu"}
                     />
                 </div>
 
-                <div className='w-full'>
+                <div className="w-full">
 
-                    <ul className='grid grid-flow-row lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 space-x-5 space-y-3 w-full p-10 items-center justify-items-center'>
+                    <ul className="grid grid-flow-row lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 space-x-5 space-y-3 w-full p-10 items-center justify-items-center">
                         {products.map(product => (
 
-                            <li key={product.sys.id} className=' flex flex-col max-w-[350px] items-center text-center'>
+                            <li key={product.sys.id} className=" flex flex-col max-w-[350px] items-center text-center">
 
-                                <Link href={'/product/' + product.fields.slug}>
-                                <Image src={'https:' + product.fields.thumbnail.fields.file.url} height={200} width={350} alt='Product Image'></Image>
-                                <h1 className=' text-wrap'>{product.fields.title}</h1>
+                                <Link href={"/product/" + product.fields.slug}>
+                                <Image src={"https:" + product.fields.thumbnail.fields.file.url} height={200} width={350} alt="Product Image"></Image>
+                                <h1 className=" text-wrap">{product.fields.title}</h1>
                                 </Link>
 
                             </li>
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import Image from 'next/image'
+import React from "react"
+import Image from "next/image"
 
 export const Hero = () => {
   return (
-    <section className='w-full h-full'>
-        <div className=' w-full h-full py-32 relative bg-primary-color bg-hero-bg bg-no-repeat bg-right-bottom flex justify-center items-center flex-col pt-14 '>
-            <div className='w-1/2 p-5'>
-                <h1 className='text-4xl md:text-5xl lg:text-6xl'>Bite Into <span className='text-secondary-color'>Happiness</span> Witch Every Mouthful</h1>
-                <p className=' text-xl md:text-3xl lg:text-4xl font-extrabold' >Juicier patties, new toastier buns and meltier cheese. The classics are now a little more 'Mmmm'.</p>
-                <button className='w-fit p-4 rounded-md text-sm text-black bg-[#ffbc0d]' >Order Now</button>    
+    <section className="w-full h-full">
+        <div className=" w-full h-full py-32 relative bg-primary-color bg-hero-bg bg-no-repeat bg-right-bottom flex justify-center items-center flex-col pt-14 ">
+            <div className="w-1/2 p-5">
+                <h1 className="text-4xl md:text-5xl lg:text-6xl">Bite Into <span className="text-secondary-color">Happiness</span> Witch Every Mouthful</h1>
+                <p className=" text-xl md:text-3xl lg:text-4xl font-extrabold" >Juicier patties, new toastier buns and meltier cheese. The classics are now a little more Mmmm.</p>
+                <button className="w-fit p-4 rounded-md text-sm text-black bg-[#ffbc0d]" >Order Now</button>    
             </div>
-            <div className='w-full absolute bottom-0'> <Image src={'/overlay.svg'} width={1} height={1} alt='overlay' className='w-full '/></div>
+            <div className="w-full absolute bottom-0"> <Image src={"/overlay.svg"} width={1} height={1} alt="overlay" className="w-full "/></div>
         </div>
         </section>
   )
